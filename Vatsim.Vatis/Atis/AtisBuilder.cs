a107ba289cd1f8ceb27aecc82d1ccda235bc213e
Justin Shannon <zla.jshannon@gmail.com>
2023-03-16 05:45:27 -0600

    Add additional prefixes to RWY parsing

--- Vatsim.Vatis/Atis/AtisBuilder.cs
+++ Vatsim.Vatis/Atis/AtisBuilder.cs
@@ -378,10 +378,11 @@ public class AtisBuilder : IAtisBuilder
         input = Regex.Replace(input, @"\bTWYS ([A-Z]{1,2}[0-9]{0,2})\b", m => $"TWYS { m.Groups[1].Value.ConvertAlphaNumericToWordGroup() }");
 
         // parse runways
-        input = Regex.Replace(input, @"\b(RY|RWY|RWYS)\s?([0-9]{1,2})([LRC]?)\b", m =>
+        input = Regex.Replace(input, @"\b(RY|RWY|RWYS|RUNWAY|RUNWAYS)\s?([0-9]{1,2})([LRC]?)\b", m =>
             StringExtensions.RwyNumbersToWords(int.Parse(m.Groups[2].Value), m.Groups[3].Value,
                 prefix: !string.IsNullOrEmpty(m.Groups[1].Value),
-                plural: !string.IsNullOrEmpty(m.Groups[1].Value) && m.Groups[1].Value == "RWYS",
+                plural: !string.IsNullOrEmpty(m.Groups[1].Value) &&
+                        (m.Groups[1].Value == "RWYS" || m.Groups[1].Value == "RUNWAYS"),
                 leadingZero: !composite.UseFaaFormat));
 
         // read numbers in group format, prefixed with # or surrounded with {}
