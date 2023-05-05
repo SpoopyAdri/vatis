da1165f64f252906d99abd913f82269bc60d669d
Justin Shannon <justin@clowd.io>
2023-05-04 22:35:47 -0600

    Bump version

--- Vatsim.Vatis/Properties/AssemblyInfo.cs
+++ Vatsim.Vatis/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.11")]
-[assembly: AssemblyInformationalVersion("4.0.0-beta.11")]
+[assembly: AssemblyVersion("4.0.0.12")]
+[assembly: AssemblyInformationalVersion("4.0.0-beta.12")]
